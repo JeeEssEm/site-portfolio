4633e27a10bb335e7e8339f66e94336cbd91c118
JeeEssEm <greendiesel228@gmail.com>
2022-09-25 17:18:59 +0300

    Добавил реальную информацию, подправил анимации и немного изменил builder

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# site-portfolio
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+# Сайт визитка
+
+### Файловое устройство
+* В папке static лежат каталоги images и css, в которых находятся изображения и файлы стилей соответственно
+* В папке scripts находятся js скрипты
+* В templates находятся html страницы
+##### Подобная структура была выбрана для будущего расширения сайт и подключения Backend без последующего масштабного изменения  Frontend части проекта
+
+
+### Использованные технологии
+* В main.js содержится скрипт, который строит html блоки для страницы portfolio.html. Это нужно для:
+  *  Упрощения заполнения страницы
+  *  Расширения сайта в будущем, например, для получения необходимого текста из базы данных и последующего построения страницы с его использованием
+* Масштабирование страниц было выполнено при помощи grid системы фреймворка Bootstrap для более удобного просмотра сайта на устройствах с разными разрешениями экрана
+* Сайт сделан в стиле glassmorphism, который был популярен в 2021 году.
--- scripts/main.js
+++ scripts/main.js
@@ -37,7 +37,6 @@ class Story {
             <div class="col-4 d-flex align-items-center justify-content-center">
                 <img src="../static/images/${this.image}" alt="" class="rounded" style="border: 4px solid #4871CB; width: 100%; height: auto">
             </div>
-       
         `
         }
 
@@ -59,7 +58,7 @@ class Story {
         if (this.miniStories.length === 0)
             return "";
 
-        let result = `<ul class=\"container col-8 dropdown-items mb-5\" id=\"mini_story_${this.id}\" style=\"height: 0\">`;
+        let result = `<ul class=\"container col-12 dropdown-items mb-5 p-5\" id=\"mini_story_${this.id}\" style=\"height: 0\">`;
         for(let i = 0; i < this.miniStories.length; i++) {
             let st = this.miniStories[i];
             result += `
@@ -103,7 +102,6 @@ function build_stories() {
 
 function dropdown_list(id) {
     let element = document.getElementById(`mini_story_${id}`);
-    // let glass = document.getElementById(`glass_${id}`);
     let arrow = document.getElementById(`arrow_${id}`);
 
     if (element.style.height === "0px")
@@ -112,7 +110,6 @@ function dropdown_list(id) {
         element.style.paddingBottom = "0.5rem"
         element.style.height = "auto"
         arrow.style.rotate = "180deg"
-        // glass.classList.remove("mb-5")
     }
 
     else {
@@ -120,35 +117,48 @@ function dropdown_list(id) {
         element.style.paddingTop = "0"
         element.style.paddingBottom = "0"
         element.style.height = "0"
-
     }
 }
 
 
 const PersonalStories = [
+
+
     {
-        img: 'maxonchick.jpg',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        theme: 'Theme name',
+        img: '2course.jpg',
+        text: 'На первом курсе Яндекс Лицея меня обучили основам языка python: синтаксис, различные встроенные функции, ООП, а также работе с файловой системой при помощи встроенных средств и разных библиотек для работы с excel файлами.',
+        theme: 'Лицей академии Яндекса. 1 год'
+    },
+
+    {
+        img: 'cert.jpg',
+        text: 'На втором курсе я реализовывал полноэкранные приложения в команде с использованием различных фреймворков, также нас познакомили с работой api и REST api, и отдельно разобрали взаимодействие с яндекс картами. Для разработки некоторых проектов мне нужно было самостоятельно разобраться в функционале библиотек для работы с компьютерными сетями. Также нас познакомили со средой github и способами взаимодейстия с ней через IDE.',
+        theme: 'Лицей академии Яндекса. 2 год',
         miniStories: [
             {
-                img: 'logo.png',
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                theme: "Veryyyyy big story naminggg"
+                img: 'messenger.png',
+                text: "Данный проект - это мессенджер, написанный на языке python, фреймворк PyQt5. Для обеспечения взаимодействия между устройствами мной был использован клиент-серверный вариант с применением библиотеки socket. Для цветового оформления интерфейса использовался css.",
+                theme: 'Первый проект. <a class="link" href="https://github.com/JeeEssEm/messenger-pyqt5">Мессенджер</a>'
             },
 
             {
-                img: 'logo.png',
-                text: "some cool story",
-                theme: "story"
+                img: 'pymogus.png',
+                text: "Данный проект - это реплика популярной игры among us. Мы написали его на языке python, фреймворк pygame. Для поддержки сетевой игры мы использовали клиент-серверный вариант с использованием библиотеки socket.",
+                theme: 'Второй проект. <a class="link" href="https://github.com/ilya159368/PygameAmongUs">Pymogus</a>'
+            },
+
+            {
+                img: 'practicehub.png',
+                text: 'Данный проект - попытка создания сайта с курсами для школы, на котором можно создать курс, записаться и пройти. На стороне frontend мы использовали html, css, и javascript, в частности JQuery. На стороне backend мы использовали язык python, фреймворк Flask и SQLAlchemy для установки ORM связи с базой данных sqlite.',
+                theme: 'Третий проект. <a class="link" href="https://github.com/ilya159368/PracticeHub">PracticeHub</a>'
             }
         ]
     },
 
-    {
-        img: 'maxonchick.jpg',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        theme: 'Theme name'
+	{
+        img: 'dreamteam.jpg',
+        text: 'После опыта с Лицеем академии Яндекса, я понял, что направление IT мне интересно, поэтому поступил в IT класс в своей школе. Здесь нас обучают основам ИИ и работе с большими данными, а также алгоритмам и структурам данных в рамках языка C++. На фото я и мои друзья из Яндекс лицея, мы работали в команде, поэтому вместе решили поступить в IT класс.',
+        theme: 'IT класс (настоящее время)'
     }
 ];
 
